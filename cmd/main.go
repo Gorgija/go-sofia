0e802c0e2ca564f36807c701f40d92def1d18e2b
Ubuntu_18 <georgievski.one@gmail.com>
2018-10-06 11:45:52 +0300

    test

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Gorgija/go-sofia/internal/diagnostics"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	log.Print("Server runing ...")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	go func() {
+		err := http.ListenAndServe(":8080", router)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}()
+
+	diagnostics := diagnostics.NewDiagnostics()
+
+	err := http.ListenAndServe(":8585", diagnostics)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
+}
